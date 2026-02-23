d8bb3bfcb46c3cdd6b086d55323d7cd66cfe1995
matosys <max@matosys.de>
2026-02-23 16:46:56 +0100

    feat(setup_features): introduce new setup feature `OpticalStrapDockingFeature`

--- src/balderhub/heart/lib/setup_features/__init__.py
+++ src/balderhub/heart/lib/setup_features/__init__.py
@@ -1,5 +1,7 @@
+from .optical_strap_docking_feature import OpticalStrapDockingFeature
 from .optical_heart_beat_feature import OpticalHeartBeatFeature
 
 __all__ = [
+    'OpticalStrapDockingFeature',
     'OpticalHeartBeatFeature',
 ]
